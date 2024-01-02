cc50ee11ca679711d752c0370d8af02f97351da3
zhaohang <478615184@qq.com>
2024-01-02 11:22:55 +0800

    动画

---
+++ Source/IronEgg/Animations/SAnimInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IRONEGG_API USAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
